b9f3f961aac67037a6c9cbfe43169f7ebf5e25ce
Reiji <me@reiji.io>
2025-01-22 18:38:58 -0400

    Added user input mode for codes.

--- main.py
+++ main.py
@@ -287,7 +287,6 @@ def fill_renban_info(driver: WebDriver, renban: Union[Dict[str, str], int]) -> N
             "Renban value is invalid, please either specify a number or a dictionary of two elements with name and address"
         )
 
-
 def start_single_ballot_process(
     driver: WebDriver, entry_url: str, **ballot_info
 ) -> None:
@@ -297,6 +296,8 @@ def start_single_ballot_process(
         sessions_to_apply_to = set(ballot_info["Sessions"])
     elif isinstance(sessions_to_apply_to, str) and sessions_to_apply_to != "All":
         sessions_to_apply_to = set([sessions_to_apply_to])
+
+    if not available_codes: available_codes = True
 
     pair = "Renban" in ballot_info
     want_goods = ballot_info.get("WantGoods", False)
@@ -309,7 +310,11 @@ def start_single_ballot_process(
     # All options are in the form of ＜xxx＞お申込み so the slice returns 'xxx'
 
     while available_codes:
-        code = available_codes.pop()
+        if available_codes is True:
+            code = input("Enter code:")
+        else:
+            code = available_codes.pop()
+
         print(f"Applying with code: {code}")
 
         # Might consider to add returning unused codes here.
@@ -352,7 +357,6 @@ def start_single_ballot_process(
                 )
                 submit_button.click()
 
-
 def main() -> None:
     with open("config.toml", "rb") as fd:
         config = tomllib.load(fd)
@@ -361,6 +365,7 @@ def main() -> None:
 
     for ballot_info in config["Ballots"]:
         start_single_ballot_process(driver, config["URL"], **ballot_info)
+
 
     driver.close()
 
