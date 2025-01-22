e33a87c172a328e70e94e433065aa1f44facfb1a
Reiji <me@reiji.io>
2025-01-22 19:48:32 -0400

    Added: - Exception Handling for wrong and invalid codes - User Input codes through console mode

--- main.py
+++ main.py
@@ -49,6 +49,18 @@ def apply_for_single_session(
     apply_button = driver.find_element(By.XPATH, "//button[text()='お申込みへ']")
     apply_button.click()
 
+    # Attempt of an exception handling method aimed to react when the code is either wrong or used.
+    time.sleep(3)
+    error_messages = driver.find_elements(By.CLASS_NAME, "varidation")
+    # print(len(error_messages))
+    if error_messages:
+        error_msg = error_messages[1].text
+        if error_msg == "利用回数を超えたためお申込みできません。":
+            print("Code " + code + " has been used for " + session_name +" before. Please check again.")
+        elif error_msg == "申し込み情報が正しくありません。":
+            print("Code " + code + " is incorrect. Please check again.")
+
+    # Waiting until the new page loads
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable(
             (
@@ -58,6 +70,7 @@ def apply_for_single_session(
         ),
     )
 
+    # Clicking Apply in first page
     apply_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable(
             (
@@ -68,6 +81,7 @@ def apply_for_single_session(
     )
     apply_button.click()
 
+    # Checking 各種注意事項に同意します on pop up
     checkbox = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable(
             (By.XPATH, "//label[text()='各種注意事項に同意します']")
@@ -75,6 +89,7 @@ def apply_for_single_session(
     )
     checkbox.click()
 
+    # Clicking Apply on popup
     apply_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable(
             (By.XPATH, "//button[text()='申込みへ' and @type='submit']")
@@ -272,7 +287,6 @@ def fill_renban_info(driver: WebDriver, renban: Union[Dict[str, str], int]) -> N
             "Renban value is invalid, please either specify a number or a dictionary of two elements with name and address"
         )
 
-
 def start_single_ballot_process(
     driver: WebDriver, entry_url: str, **ballot_info
 ) -> None:
@@ -282,6 +296,8 @@ def start_single_ballot_process(
         sessions_to_apply_to = set(ballot_info["Sessions"])
     elif isinstance(sessions_to_apply_to, str) and sessions_to_apply_to != "All":
         sessions_to_apply_to = set([sessions_to_apply_to])
+
+    if not available_codes: available_codes = True
 
     pair = "Renban" in ballot_info
     want_goods = ballot_info.get("WantGoods", False)
@@ -294,8 +310,14 @@ def start_single_ballot_process(
     # All options are in the form of ＜xxx＞お申込み so the slice returns 'xxx'
 
     while available_codes:
-        code = available_codes.pop()
+        if available_codes is True:
+            code = input("Enter code:")
+        else:
+            code = available_codes.pop()
+
         print(f"Applying with code: {code}")
+
+        # Might consider to add returning unused codes here.
 
         for session_name in sessions_name:
             if sessions_to_apply_to == "All" or session_name in sessions_to_apply_to:
@@ -335,7 +357,6 @@ def start_single_ballot_process(
                 )
                 submit_button.click()
 
-
 def main() -> None:
     with open("config.toml", "rb") as fd:
         config = tomllib.load(fd)
@@ -344,6 +365,7 @@ def main() -> None:
 
     for ballot_info in config["Ballots"]:
         start_single_ballot_process(driver, config["URL"], **ballot_info)
+
 
     driver.close()
 
